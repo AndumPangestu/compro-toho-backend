313737163524805631f6eadc5aeef9a5eea39bea
Agung Kusaeri <agung.kusaeri9@gmail.com>
2025-08-05 10:59:58 +0700

    delete filemanager

--- routes/web.php
+++ routes/web.php
@@ -304,6 +304,3 @@ Route::group(['middleware' => ['auth', 'admin']], function () {
         Route::delete('/{id}', [ContactController::class, 'destroy'])->name('contact.destroy');
     });
 });
-Route::group(['prefix' => 'filemanager', 'middleware' => ['web', 'auth']], function () {
-    \UniSharp\LaravelFilemanager\Lfm::routes();
-});
